04aa311b654d041d5c524632ec59878963f1c4f0
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-12-18 15:27:54 -0800

    Remove build

--- packages/components/src/components.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Components {
-    get(id) {
-        return id;
-    }
-}
-exports.Components = Components;
-//# sourceMappingURL=components.js.map
--- packages/tracking/src/tracking.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Tracking {
-    get(id) {
-        return id;
-    }
-}
-exports.Tracking = Tracking;
-//# sourceMappingURL=tracking.js.map
